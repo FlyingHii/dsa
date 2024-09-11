febaa35f35f071b9ee8a17b75ba528c67614fbd1
hai-vo <vovanthanhhai.96@gmail.com>
2024-09-12 00:44:25 +0700

    update

--- go/leetcode.go
+++ go/leetcode.go
@@ -15,3 +15,53 @@ func mergeAlternately(word1 string, word2 string) string {
 	}
 	return string(retByte)
 }
+
+// 345
+/**
+xx xx xx xx
+*/
+func reverseVowels(s string) string {
+	vowels := map[byte]bool{
+		'a': true, 'e': true, 'i': true, 'o': true, 'u': true,
+		'A': true, 'E': true, 'I': true, 'O': true, 'U': true,
+	} // use map (for create set) for quick lookup
+	left := 0
+	right := len(s) - 1
+	runes := []byte(s) // use byte instead of rune
+	for left < right {
+		if !vowels[runes[right]] {
+			right--
+			continue
+		}
+
+		if !vowels[runes[left]] {
+			left++
+			continue
+		}
+		runes[right], runes[left] = runes[left], runes[right]
+		right--
+		left++
+	}
+	return string(runes)
+}
+
+// 151
+func reverseWords(s string) string {
+	var words []byte
+	var word []byte
+	for i := 0; i < len(s); i++ {
+		if (s)[i] != ' ' {
+			word = append(word, (s)[i])
+		}
+
+		if ((s)[i] == ' ' || i == len(s)-1) && len(word) != 0 {
+			if len(words) > 0 {
+				words = append([]byte{' '}, words...)
+			}
+			words = append(word, words...)
+			word = []byte{}
+		}
+	}
+
+	return string(words)
+}
