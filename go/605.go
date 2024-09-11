7c062c6fd9c291e565d77db8ef71478513cc0991
hai-vo <vovanthanhhai.96@gmail.com>
2024-09-11 11:50:07 +0700

    update

--- go/605.go
+++ go/605.go
@@ -1,5 +1,27 @@
 package _go
 
 func canPlaceFlowers(flowerbed []int, n int) bool {
-
+	var (
+		emptySpace int8 = 0
+		plantedFl  int  = 0
+	)
+	for i, flb := range flowerbed {
+		if i == 0 {
+			emptySpace++
+		}
+		if i == len(flowerbed)-1 {
+			emptySpace++
+		}
+		if flb == 0 && emptySpace < 3 {
+			emptySpace++
+		}
+		if flb == 1 {
+			emptySpace = 0
+		}
+		if emptySpace == 3 {
+			plantedFl++
+			emptySpace = 1
+		}
+	}
+	return plantedFl >= n
 }
